475513cb9b89b405879aecee0d92d9ef45cea706
SeanTangney <sean_tangney@hotmail.com>
2022-08-23 14:16:12 +0000

    Added basc checkout functionality

--- cul/settings.py
+++ cul/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
     # Other
     'crispy_forms',
+    'stripe',
     ]
 
 MIDDLEWARE = [
@@ -171,5 +172,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+# Stripe
+
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
