492fc6c0323e392a0c7c168f5894f2a32e61791b
SeanTangney <sean_tangney@hotmail.com>
2022-08-24 10:16:03 +0000

    Added .jshinrc to specify use of version es6

--- checkout/models.py
+++ checkout/models.py
@@ -20,9 +20,12 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=60, null=True, blank=True)
     county = models.CharField(max_length=20, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
-    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    delivery_cost = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=False, default=0)
 
     def _generate_order_number(self):
         """
@@ -36,7 +39,8 @@ class Order(models.Model):
         accounting for the delivery costs
         """
 
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        self.order_total = self.lineitems.aggregate(
+            Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
@@ -46,7 +50,8 @@ class Order(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Override original save method to set order number if it hasnt been set already
+        Override original save method to set
+        order number if it hasnt been set already
         """
 
         if not self.order_number:
@@ -58,15 +63,20 @@ class Order(models.Model):
 
 
 class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
-    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    product_size = models.CharField(max_length=2, null=True, blank=True)  # XS -> XL
+    order = models.ForeignKey(
+        Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(
+        Product, null=False, blank=False, on_delete=models.CASCADE)
+    product_size = models.CharField(
+        max_length=2, null=True, blank=True)  # XS -> XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
-    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+    lineitem_total = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
         """
-        Override original save method to set order number if it hasnt been set already
+        Override original save method to set
+        order number if it hasnt been set already
         """
 
         self.lineitem_total = self.product.price * self.quantity
