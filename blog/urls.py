b75d0116e5dae5593e11f2ed20ba6bba3bfe15ce
SeanTangney <sean_tangney@hotmail.com>
2022-08-31 17:54:00 +0000

    Add delete blog post functionality

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import CulBlog, BlogDetailView, AddBlogView
+from .views import CulBlog, BlogDetailView, AddBlogView, UpdateBlogView, DeleteBlogView
 
 urlpatterns = [
     path('', CulBlog.as_view(), name="blog"),
     path('blog_detail/<int:pk>', BlogDetailView.as_view(), name="blog_detail"),
-    path('add_blog/', AddBlogView.as_view(), name="add_blog"),
+    path('add/', AddBlogView.as_view(), name="add_blog"),
+    path('blog-detail/update/<int:pk>', UpdateBlogView.as_view(), name="update_blog"),
+    path('blog-detail/detail/<int:pk>', DeleteBlogView.as_view(), name="delete_blog"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm
+from django.urls import reverse_lazy
 
 
 class CulBlog(ListView):
     model = Post
     template_name = 'blog/blog.html'
+    paginate_by = 5
 
 
 class BlogDetailView(DetailView):
@@ -17,3 +19,19 @@ class AddBlogView(CreateView):
     model = Post
     form_class = PostForm
     template_name = 'blog/add_blog.html'
+
+
+class UpdateBlogView(UpdateView):
+    model = Post
+    template_name = 'blog/update_blog.html'
+    fields = [
+        'image',
+        'title',
+        'body',
+    ]
+
+
+class DeleteBlogView(DeleteView):
+    model = Post
+    template_name = 'blog/delete_blog.html'
+    success_url = reverse_lazy('blog')
