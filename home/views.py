f8dce58d81979032adbe19c23ec7c42eb493888f
SeanTangney <sean_tangney@hotmail.com>
2022-09-06 13:39:32 +0000

    Add Contact us form to the footer

--- home/views.py
+++ home/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def contact(request):
+    """ A view send a contact form to cul sports """
+    if request.method == "POST":
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+
+        messages.success(request, 'Thank you. '
+                                  ' Message Succesfully Sent!')
+        return render(request, 'home/contact.html')
+
+    else:
+        messages.error(request, 'Failed To Send Message. '
+                                ' Please Ensure The Form Is Valid.')
+        return render(request, 'home/contact.html')
